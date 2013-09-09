87a9c2dfe373b2eee8e67db0e6afac9da9d2646e
Michael Pellauer <michael.i.pellauer@intel.com>
2013-09-09 21:14:42 +0000

    CSN: CSN-leap-examples-33
    Fixup some license headers.

--- modules/leap/libli/li-hybrid-app/li-hybrid-app-null-hw.bsv
+++ modules/leap/libli/li-hybrid-app/li-hybrid-app-null-hw.bsv
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2008 Intel Corporation
+// Copyright (C) 2013 Intel Corporation
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -14,6 +14,13 @@
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
+//
+// @file li-hybrid-app-null-hw.cpp
+// @brief LI Hybrid App Empty Hardware
+//
+// @author Michael Pellauer
 //
 
 import Vector::*;
--- modules/leap/libli/riso/riso.cpp
+++ modules/leap/libli/riso/riso.cpp
@@ -1,12 +1,27 @@
-/*****************************************************************************
- *
- * @author daniel.j.lustig,11339344
- *
- * Copyright (c) 2013 Intel Corporation, all rights reserved.
- * THIS PROGRAM IS AN UNPUBLISHED WORK FULLY PROTECTED BY COPYRIGHT LAWS AND
- * IS CONSIDERED A TRADE SECRET BELONGING TO THE INTEL CORPORATION.
- *
- *****************************************************************************/
+//
+// Copyright (C) 2013 Intel Corporation
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+
+//
+// @file riso.cpp
+// @brief LI RISO App
+//
+// @author Daniel Lustig
+//
 
 #include <cstdlib>
 #include <iostream>
